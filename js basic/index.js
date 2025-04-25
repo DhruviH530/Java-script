a3a4326b645eb44bb97bc8addd1eaecb1de8d07b
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-04-25 15:27:53 +0530

    Add files via upload

---
+++ js basic/index.js
@@ -0,0 +1,23 @@
+// line is use to print on the console window 
+console.log('hii dhv 1');
+
+// let a = 'true';
+// console.log(a);
+
+
+// const num =12;
+// num =13;
+
+// let lastName ='desai';
+
+// console.log(lastName);
+
+// lastName =true; 
+// console.log(lastName);
+
+
+
+let age = 17;
+let status = (age >= 18) ? 'Yes vote' : 'No vote';
+
+console.log(status);

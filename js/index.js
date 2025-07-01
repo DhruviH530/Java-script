27b40b6ec7c280f246b4d9341799bf0be01999b5
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-07-01 22:26:24 +0530

    basic (var , let , const ) (for loop )

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+let a = 1;
+ a = 2;
+console.log(a);
+ 
+// // using const 
+// // it will thorw a nd error because we cnat change the value once we declare
+//                 // const a = 1;
+//                 //  a = 2;
+//                 // console.log(a);
+    
+
+
+
+// // 
+
+// // let firstName = "dhruvi";
+// // let age = "22";
+// // let isMarried = "no ";
+
+// // console.log("this persone name is" + firstName +" her age is " + age + isMarried + "she is not married" );
+
+
+
+// // priniting firstname and last name in dumb way 
+
+let firstName = "dhruvi";
+let lastName = "desai";
+
+console.log(firstName + lastName);
+
+
+// // loops 
+
+
+
+// let firstName = "dhruvi";
+// let age = 22 ;
+// let isMarried = "false ";
+
+
+// // if/else
+
+if(isMarried == true){
+    console.log(firstName + "is married");
+}
+else{
+    
+    console.log(firstName + "is not married");
+}
+
+
+// print 1 to 100 by for loop 
+let answer = 0
+
+for(let i = 0; i<=100; i++){
+        answer= answer + i;
+}
+console.log(answer);

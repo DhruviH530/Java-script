8e972fc41bf0299433aa4b5b6b2a9513620ad075
Dhruvi Desai <desaidhruvi345@gmail.com>
2025-07-03 22:55:13 +0530

    date

--- js/index.js
+++ js/index.js
@@ -1,90 +1,47 @@
 
 
 
-// // male na name nikalvana che
- 
-const personArray = ["prem" ,"kirat" , "harsh" ,"dhruvi"];
-const genderArray = ["male" , "male" , "male" ,"female"];
+// ex
 
+class animal {
+    constructor(name , legCount ,speaks){
+      this.name = name;
+      this.legCount = legCount;
+      this.speaks = speaks;
+    }
 
-
-
-for(let i = 0; i<personArray.length; i++){
-    if(genderArray[i] == "male"){
-        console.log(personArray[i]);
+    speak(){
+      console.log("hi there " + this.speaks);
     }
 }
 
 
-// // functions
+let dog = new animal ("dog" , 4 , "bhao bhao");
+let cat = new animal ("cat" , 4 , "mewo");
+cat.speak();
 
-function sum (a ,b){
-    //  input and return is output
-    return a+b ;
+
+// Date 
+
+const currentDate = new Date();
+
+console.log(currentDate.getDate());
+console.log(currentDate.getMonth()+1);
+console.log(currentDate.getFullYear());
+
+
+
+// milisec
+
+// const currentDate = new Date();
+
+console.log("time in milisecound since 1970:", currentDate.getTime());
+
+
+// ex
+
+function currentTimePrint(){
+  console.log(new Date().getMinutes());
 }
 
-// const value = sum(1 , 2)
-// console.log(value);
-
-
-// call back function
-
-function sum(num1, num2) {
-let result = num1 + num2;
-return result;
-}
-
-function displayResult(data) {
-console.log("Result of the sum is : " + data);
-return data;
-}
-
-function displayResultPassive(data) {
-console.log("Sum's result is : " + data);
-}
-
-console.log(displayResult(sum(3, 5)));
-
-function calculateArithmetic(a ,b , type){
-        if(type == "sum"){
-            const value = sum(a, b);
-            return value;
-        }
-
-        if(type == "minus"){
-            const value = sub(a , b);
-            return value;
-
-        }
-}
-
- function sum( a, b){
-    return a + b;
- }
-
- function sub(a,b){
-    return a - b;
- }
-
- const value = calculateArithmetic (1,2, "minus");
- console.log(value);
-
-
-
-
-
-//  setTimeOut
-
-    function greet(a , b){
-        console.log("hello dhruvi");
-    }
-
-    setTimeout(greet , 1 * 1000);
-
-// setInterval
-
-        function greet(a , b){
-        console.log("hello dhruvi");
-    }
-
-    setInterval(greet , 1 * 1000);
+setTimeout(currentTimePrint, 1000);
